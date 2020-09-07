53bd7d15c629e25e0e788bae498d8ac6c1143f05
gomesIgor21 <igorb.21gomes@gmail.com>
2020-09-07 15:37:56 -0300

    Class Uniao_federeativa done

--- dadospessoais/src/modelo/entidades/Uniao_federativa.java
+++ dadospessoais/src/modelo/entidades/Uniao_federativa.java
@@ -1,5 +1,67 @@
 package modelo.entidades;
 
-public class Uniao_federativa {
+import java.io.Serializable;
 
+public class Uniao_federativa implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome_uf;
+	
+	public Uniao_federativa() {
+	}
+
+	public Uniao_federativa(Integer id, String nome_uf) {
+		super();
+		this.id = id;
+		this.nome_uf = nome_uf;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome_uf() {
+		return nome_uf;
+	}
+
+	public void setNome_uf(String nome_uf) {
+		this.nome_uf = nome_uf;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Uniao_federativa other = (Uniao_federativa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Uniao_federativa [id=" + id + ", nome_uf=" + nome_uf + "]";
+	}
+	
 }
