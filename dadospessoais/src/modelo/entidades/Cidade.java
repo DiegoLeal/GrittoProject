2e5768e9a1ca861573539e14690a7831108ca2e2
gomesIgor21 <igorb.21gomes@gmail.com>
2020-09-07 15:35:26 -0300

    Class Cidade done

--- dadospessoais/src/modelo/entidades/Cidade.java
+++ dadospessoais/src/modelo/entidades/Cidade.java
@@ -1,5 +1,77 @@
 package modelo.entidades;
 
-public class Cidade {
+import java.io.Serializable;
+
+public class Cidade implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private Uniao_federativa uf;
+	
+	public Cidade() {
+	}
+
+	public Cidade(Integer id, String nome, Uniao_federativa uf) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.uf = uf;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Uniao_federativa getUf() {
+		return uf;
+	}
+
+	public void setUf(Uniao_federativa uf) {
+		this.uf = uf;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cidade [id=" + id + ", nome=" + nome + ", uf=" + uf + "]";
+	}
 	
 }
