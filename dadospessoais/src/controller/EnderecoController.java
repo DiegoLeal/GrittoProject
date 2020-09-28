28e1dbeb2f1a2fd7a6bfd55a13ff9a3a71d2a567
gomesIgor21 <igorb.21gomes@gmail.com>
2020-09-28 14:25:39 -0300

    Endereco Controller Create method

---
+++ dadospessoais/src/controller/EnderecoController.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.sql.SQLException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import modelo.dao.DaoFactory;
+import modelo.dao.EnderecoDao;
+import modelo.dao.impl.EnderecoDaoJDBC;
+import modelo.entidades.Bairro;
+import modelo.entidades.Cidade;
+import modelo.entidades.Endereco;
+import modelo.entidades.Rua;
+
+public class EnderecoController {
+	
+	private Endereco jsonToEndereco(JSONObject json) {
+		Endereco endereco = new Endereco();
+		Bairro bairro = new Bairro();
+		Cidade cidade = new Cidade();
+		Rua rua = new Rua();
+		
+		try {
+			endereco.setId(json.getInt("id"));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		cidade.setId(json.getInt("cidade_id"));
+		endereco.setCidade(cidade);
+		bairro.setId(json.getInt("bairro_id"));
+		endereco.setBairro(bairro);
+		rua.setId(json.getInt("rua_id"));
+		endereco.setRua(rua);
+		endereco.setCep(json.getString("cep"));
+		return endereco;
+	}
+	
+	private JSONObject enderecoToJson(Endereco endereco) {
+		JSONObject json = new JSONObject();
+		
+		json.put("id", endereco.getId());
+		json.put("cidade_id", endereco.getCidade().getId());
+		json.put("bairro_id", endereco.getBairro().getId());
+		json.put("rua_id", endereco.getRua().getId());
+		json.put("cep", endereco.getCep());
+		
+		return json;
+	}
+	
+	private JSONObject Create(JSONObject json) throws SQLException, JSONException {
+		Endereco endereco = jsonToEndereco(json);
+		EnderecoDao dao = DaoFactory.createEnderecoDao();
+		
+		dao.insert(endereco);
+		
+		return enderecoToJson(endereco);
+	}
+}
